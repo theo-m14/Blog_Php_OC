11060d64cf7b7727f900584079227a555d062cf6
Michel Theo <theo.michel2@outlook.fr>
2023-04-21 14:58:30 +0200

    create homepage route, implements findroute method

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -15,5 +15,26 @@ class Router
     
     public function findRoute(HttpRequest $httpRequest)
     {
+        $url = $httpRequest->getUrl();
+        $method = $httpRequest->getMethod();
+        $routeFound = array_filter($this->listRoute,function($route) use ($url,$method){
+            if(str_contains($route->path,'['))
+                return preg_match("#^" . $route->path . "$#", $url) && $route->method == $method;
+        });
+        $numberRoute = count($routeFound);
+        if($numberRoute > 1)
+        {
+            //Implement Exception
+            print("Too many Route found");
+        }
+        else if($numberRoute == 0)
+        {
+            //Implement Exception
+            print("No route found");
+        }
+        else
+        {
+            return new Route(array_shift($routeFound));	
+        }
     }
 }
